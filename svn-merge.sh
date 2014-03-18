62cdf40508d02c3b9702df474af8f2ed774390b0
poluria <kjlee@kjlee-pc.(none)>
2014-03-18 18:32:33 +0900

    remove unnecessary echo.

--- svn-merge.sh
+++ svn-merge.sh
@@ -10,8 +10,6 @@ MODULES=${@:2}
 REPO_ROOT=`LANG=C; svn info $2 | grep "Repository Root" | sed 's/Repository Root: //'`
 LOG_MESSAGE_FILE=log_message.tmp
 
-echo $LANG
-
 if [ ! -f $LOG_MESSAGE_FILE ];then
 	echo "Merged from trunk" > $LOG_MESSAGE_FILE
 fi
